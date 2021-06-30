51cda634ab47c504f2f7f6e5522a5330e5621c0e
ksanchezm24 <karenssanchezm@gmail.com>
2021-06-29 21:06:21 -0500

    Second Class, java coding

--- src/App.java
+++ src/App.java
@@ -15,5 +15,60 @@ public class App {
 
     public static void main(String[] args) throws Exception {
         System.out.println("Hello World!");
+        // Tipos de Datos y Operadores
+        String res= "Hola";
+        int resultado = 15+32;
+        int resultado3= 125 - 32;
+        double resultado2= 25/3;
+        float resultado4= 58*21;
+        int resultado5= 21%9;
+
+        //Operadores Unarios: Permiten trabajar con un solo valor
+        //el ++ toma el valor inicial de la variable y lo aumenta en uno
+        // el -- resta 1
+        // el -variable, convierte la variable a valores negativos
+        // el +variable, convierte la variable a positiva
+        resultado++;
+        resultado--;
+        resultado = -resultado;
+        resultado = +resultado;
+
+        //Operadores Logicos
+        boolean respuesta = true;
+        respuesta = 5 > 8;
+        respuesta = 5 < 8;
+        respuesta = 5 <= 8;
+        respuesta = 5 >= 8;
+        respuesta = 5 != 8; // NOT
+        respuesta = 5 == 8;
+
+        //Conectores
+        
+        respuesta = 12 < 5 && 4 > 1;
+        respuesta = 14 < 25 || 1 > 5;
+
+        //Operadores Bit a Bit
+        // &
+        // ^ comparando dos expresiones lógicas 
+        // | Hace algunas operaciones
+
+
+        // Estructuras de Control
+
+        if (respuesta) {
+            //Bloque de codigo
+            
+        }
+        if (respuesta) {
+            //bloque de codigo si se cumple
+            
+        } else if (respuesta) {
+            
+        } {
+            //bloque de codigo si no se cumple
+            
+        }
+
+
     }
 }

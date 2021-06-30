381a764e2ef19018713c5cf958215bc9c3db5fcf
ksanchezm24 <karenssanchezm@gmail.com>
2021-06-29 21:01:35 -0500

    Second Class, java coding

--- src/App.java
+++ src/App.java
@@ -1,5 +1,19 @@
 public class App {
+
+    private String nombre;
+    private int edad;
+    private char sexo; 
+    // char guarda solo 1 letra.
+    private double estatura;
+    // double es para numero con decimales
+    private float peso;
+    // En java es mejor usar las herramientas estandar para defiir las variables, se coloca la primera en minuscula y la segunta en mayuscula
+    private boolean estadoCivil;
+
+
+
+
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println("Hello World!");
     }
 }
